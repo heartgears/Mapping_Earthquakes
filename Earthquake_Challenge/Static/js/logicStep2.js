e65e1c05d440e6aad06c52faa1050dc2cd6ab3c3
Hunter Gierhart <102868909+heartgears@users.noreply.github.com>
2022-07-09 12:01:30 -0400

    Add files via upload

---
+++ Earthquake_Challenge/Static/js/logicStep2.js
@@ -0,0 +1,128 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, 98.5],
+  zoom: 3,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  
+  pointToLayer: function(feature, latlng) {
+              console.log(data);
+              return L.circleMarker(latlng);
+          },
+        // We set the style for each circleMarker using our styleInfo function.
+      style: styleInfo
+      }).addTo(map);
+  });
+
+/* // Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522, -118.2437], 14); */
+
+/* // An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+// Get data from cities.js
+let cityData = cities; */
+
+/* // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circlemarker(city.location)
+    radius: city.population/100000
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+}); */
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
